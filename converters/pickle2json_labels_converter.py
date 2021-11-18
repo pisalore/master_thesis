82ef82b30536ebf2e605f99801a7fa571f4ebb26
pisalore <lorenzo.pisaneschi@gmail.com>
2021-11-18 18:31:42 +0100

    Add dataset split in train and validation sets

--- converters/pickle2json_labels_converter.py
+++ converters/pickle2json_labels_converter.py
@@ -9,20 +9,30 @@ import re
 
 
 def generate_json_labels(pickle_file, png_path):
+    """
+    Convert annotations from parsing pipeline to COCO annotation, in order to use GANs
+    :param pickle_file: contains the dict with all the papers an the objects inside them
+    :param png_path: path to generated png files (annotated via OpenCV)
+    :return: train and validation json annotations
+    """
     annotation_id = 1000000
     is_crowd = 0
-    json_annotations = {"images": [], "annotations": [], "categories": {}}
+    json_annotations = {"train": {"images": [], "annotations": [], "categories": {}},
+                        "val": {"images": [], "annotations": [], "categories": {}}}
     docs_instances = load_doc_instances(pickle_file)
+    papers_num = len(docs_instances.keys())
+    train_papers_num = int(papers_num / 100 * 90)
     # for paper_key in ["5KGUvlXI6pKGkLqBu3YZ5", "1DW2ueDjdXU6kHDZq0Tg3z", "622U0zHbbvLmKLHzTHo6gZ"]:
-    for paper_key in docs_instances:
+    for paper_idx, paper_key in enumerate(docs_instances):
+        key = "train" if paper_idx < train_papers_num else "val"
         title = docs_instances.get(paper_key).get("title")
         abstract = docs_instances.get(paper_key).get("abstract")
         # Save images for a key (paper)
-        for idx, page_img_path in enumerate(Path(png_path).rglob(f'{paper_key}_*.png')):
+        for page_idx, page_img_path in enumerate(Path(png_path).rglob(f'{paper_key}_*.png')):
             parts = page_img_path.parts
             page_img_path = PurePosixPath(pathlib.Path(*parts[2:]))
             image_id = int(re.sub('\D', '', page_img_path.stem))
-            json_annotations["images"].append(
+            json_annotations[key]["images"].append(
                 {
                     "file_name": page_img_path.__str__(),
                     "heigth": 792,
@@ -30,14 +40,14 @@ def generate_json_labels(pickle_file, png_path):
                     "width": 612,
                 })
             # Save annotations for the first page (title and abstract, for the moment)
-            if idx == 0:
+            if page_idx == 0:
                 if title:
-                    json_annotations["annotations"].append(
+                    json_annotations[key]["annotations"].append(
                         {
                             "segmentation": calculate_segmentation(title.get("coords")),
                             "area": calculate_area(title.get("coords")),
                             "is_crowd": is_crowd,
-                            "imaged_id": image_id,
+                            "image_id": image_id,
                             "bbox": generate_coco_bbox(title.get("coords")),
                             "category_id": CATEGORIES_MAP.get("title"),
                             "id": annotation_id
@@ -45,12 +55,12 @@ def generate_json_labels(pickle_file, png_path):
                     )
                     annotation_id += 1
                 if abstract:
-                    json_annotations["annotations"].append(
+                    json_annotations[key]["annotations"].append(
                         {
                             "segmentation": calculate_segmentation(abstract.get("coords")),
                             "area": calculate_area(abstract.get("coords")),
                             "is_crowd": is_crowd,
-                            "imaged_id": image_id,
+                            "image_id": image_id,
                             "bbox": generate_coco_bbox(abstract.get("coords")),
                             "category_id": CATEGORIES_MAP.get("abstract"),
                             "id": annotation_id
@@ -61,16 +71,16 @@ def generate_json_labels(pickle_file, png_path):
                 # title and abstract already handled, since they have a different data structure
                 if category not in ["title", "abstract"]:
                     # ann object is the annotated object for that page
-                    ann_object = docs_instances.get(paper_key).get(category).get(idx + 1)
+                    ann_object = docs_instances.get(paper_key).get(category).get(page_idx + 1)
                     if ann_object:
                         for i in range(len(ann_object)):
                             coords = ann_object[i].get("coords")
-                            json_annotations["annotations"].append(
+                            json_annotations[key]["annotations"].append(
                                 {
                                     "segmentation": calculate_segmentation(coords),
                                     "area": calculate_area(coords),
                                     "is_crowd": is_crowd,
-                                    "imaged_id": image_id,
+                                    "image_id": image_id,
                                     "bbox": generate_coco_bbox(coords),
                                     "category_id": CATEGORIES_MAP.get(category),
                                     "id": annotation_id
@@ -79,6 +89,9 @@ def generate_json_labels(pickle_file, png_path):
                             annotation_id += 1
         print(f"Processed paper n°: {paper_key}")
     # Dump json file
-    json_annotations["categories"] = CATEGORIES.get("categories")
+    json_annotations["train"]["categories"] = CATEGORIES.get("categories")
+    json_annotations["val"]["categories"] = CATEGORIES.get("categories")
     with open('train.json', 'w') as fp:
-        json.dump(json_annotations, fp)
+        json.dump(json_annotations["train"], fp)
+    with open('val.json', 'w') as fp:
+        json.dump(json_annotations["val"], fp)
