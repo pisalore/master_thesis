2138b36967fb579735301811fd16a7649932f4b2
pisalore <lorenzo.pisaneschi@gmail.com>
2022-01-18 10:53:06 +0100

    Add minor fix and all kind of scientific papers text generation

--- generators/text_generators.py
+++ generators/text_generators.py
@@ -32,31 +32,39 @@ def generate_random_text(pickle_file, category, text_num):
     :param text_num: the number of text objects to be created
     """
     papers_instances = load_doc_instances(pickle_file)
-    all_text, all_text_lengths = [], []
+    all_text, all_text_lengths, text = [], [], ""
     for paper in papers_instances.keys():
         # Get category text and save it, along with text length in order to calculate the mean of words to use
         # for text generation
         instance = papers_instances.get(paper).get(category)
-        text = instance.get("content") if category != "keywords" else instance.get("terms")
-        if text:
-            if category == "keywords":
-                text = ",".join(text)
-            if category == "abstract":
-                text = f"Abstract- {text}"
+        if category == "title" and instance.get('content'):
+            text = instance.get('content')
+        elif category in ["keywords", "authors"] and instance.get("terms"):
+            text = ", ".join(instance.get("terms"))
+        elif category == "subtitles" and instance.get("titles_contents"):
+            text = ", ".join(instance.get("titles_contents"))
+        elif category == "abstract" and instance.get("content"):
+            text = f"Abstract- {instance.get('content')}"
+        elif category == "text":
+            for page in instance.keys():
+                for content in instance[page]:
+                    if content.get("content"):
+                        all_text.append(content.get("content"))
+        if text and category != "text":
             all_text.append(text)
-            all_text_lengths.append(len(text.split(" " if category != "keywords" else ", ")))
+            # all_text_lengths.append(len(text.split(" " if category not in ["keywords", "authors", "subtitles"] else ",")))
 
     # Tokenize the text.
     corpus = [word_tokenize(s) for s in all_text]
     # Preprocess the tokenized text for 3-grams language modelling
     n = 3
-    num_words = randrange(0, len(all_text_lengths))
     train_data, padded_sents = padded_everygram_pipeline(n, corpus)
     model = MLE(n)
     model.fit(train_data, padded_sents)
 
     for i in range(text_num):
-        print(f"{generate_sent(model, 30, random_seed=randint(1, 150))}")
+        # num_words = all_text_lengths[randrange(0, len(all_text_lengths))]
+        print(f"{generate_sent(model, 100, random_seed=randint(1, 150))}")
 
 
-generate_random_text("../docs_instances.pickle", "keywords", 10)
+generate_random_text("../docs_instances.pickle", "text", 200)
